861150bbbda9e36843dd11f63b73aecacc858e72
Rakib0101 <rakib.01jan@gmail.com>
2021-10-31 12:34:47 +0600

    delete

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const port = process.env.PORT || 5000
 //middleware
 app.use(cors())
 app.use(express.json())
+
 const uri =
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gzsrh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -90,6 +91,13 @@ async function run() {
             res.json(result);
         });
 
+        //delete api for orders
+        app.delete("/orders/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orders.deleteOne(query);
+            res.json(result);
+        });
         //post api for orders
         app.post("/orders", async (req, res) => {
             const order = req.body;
